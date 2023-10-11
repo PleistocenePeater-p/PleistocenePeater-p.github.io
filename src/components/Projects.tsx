3254ddd5a7e449a30f7a15e642c19676b7d64c92
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-10-11 14:09:11 -0700

    Projects are their own component, destructured title, image, tech used, url

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,25 +1,35 @@
-import Image from 'next/image'
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import ProjectItem from './ProjectItem'
 import novelnest from 'public/assets/projects/novelnest.png'
 import kavorka from 'public/assets/projects/kavorka.png'
 
 function Projects() {
   return (
-    <div className='w-full'>
-        <div className='max-w-[1240px] mx-auto px-2 py-16'>
-            <p className='text-xl tracking-widest uppercase text-[#31bab1]'>Projects</p>
-            <h2 className='py-4'>What I've Built</h2>
-            <div className='grid md:grid-cols-2 gap-8'>
-                <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-300 rounded-xl p-4 group hover:bg-gradient-to-r from-[#31bab1] to-[#34ebdb]'>
-                    <Image src={novelnest} alt='/' />
-                </div>
-                <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-300 rounded-xl p-4 group hover:bg-gradient-to-r from-[#31bab1] to-[#34ebdb]'>
-                    <Image src={kavorka} alt='/' />
-                </div>
-            </div>
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#31bab1]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title='Novel Nest'
+            backgroundImg={novelnest}
+            builtWith='Python | Django | PostgreSQL'
+            projectUrl="/novelnest"
+          />
+          <ProjectItem
+            title='Kavorka'
+            backgroundImg={kavorka}
+            builtWith='MongoDB | Express | React | Node'
+            projectUrl="/kavorka"
+          />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Projects
