c302ba4919959c0ff35b469253428ea3de6ab7b4
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-11-02 12:27:20 -0700

    Styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,10 +8,10 @@ export default function Main() {
   return (
     <div id='home' className='w-full h-screen text-center'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-            <div>
+            <div className='mx-8'>
                 <p className='uppercase text-sm tracking-widest text-gray-600'>Let's build.</p>
                 <h1 className='py-4 text-gray-700'>My name is Alex Hughes.</h1>
-                <p>I'm a freelance developer with a passion for efficiency and clean design. I use Next.js and Tailwind to bring ideas to life, while constantly acquiring new skills to create better products.</p>
+                <p className='mx-8'>I'm a freelance developer with a passion for efficiency and clean design. I use Next.js and Tailwind CSS to bring ideas to life, while constantly acquiring new skills to create better products.</p>
                 <div className='flex items-center justify-center space-x-6 max-w-[330px] m-auto mt-4 py-4'>
                     <Link href='https://www.linkedin.com/in/alex-hughes-ah77/'>
                         <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
